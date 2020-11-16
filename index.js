8558e1623801513a5a325c7c65865acd3b27667f
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2020-11-16 19:43:33 +0100

    updating shit

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function dropDownMenu() {
-    document.getElementsByClassName("dropbutton").classList.toggle("show");
+    document.getElementById("dropdown").classList.toggle("show");
   }
   
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbutton')) {
-      var dropdowns = document.getElementById("dropdown");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+      const dropdown = document.getElementById("dropdown");
+        if (dropdown.classList.contains('show')) {
+          dropdown.classList.remove('show');
         }
-      }
     }
   }
