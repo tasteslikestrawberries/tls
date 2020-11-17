2430078708d0df24a29ec2ff01556e200c837a21
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2020-11-17 19:13:46 +0100

    updating

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function dropDownMenu() {
-    document.getElementById("dropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbutton')) {
-      const dropdown = document.getElementById("dropdown");
-        if (dropdown.classList.contains('show')) {
-          dropdown.classList.remove('show');
+function dropDownMenuShow(event) {
+  console.log(event.target)
+  console.log(event.currentTarget)
+    var dropdown = document.getElementById("dropdown");
+    var dropdownClasses = dropdown.className.split(" ");
+    var index = dropdownClasses.indexOf("show");
+    //console.log(target.className === "dropbutton");
+    //console.log(event.target);
+    //console.log(index);
+    if (index < 0 && event.target.className === "dropbutton") {
+      dropdown.classList.toggle("show");
+      //console.log(index);
+    } else if (index >= 0) {
+      //console.log(event.target.className);
+      if (event.target.className !== "dropbutton" && 
+        event.target.className !== "dropdownContent") {
+          dropdown.classList.toggle("show");      
         }
     }
   }
+
+  function dropDownMenuHide(event) {
+    console.log(event.target);
+    var dropdown = document.getElementById("dropdown");
+    if (event.target.className !== "dropbutton" && 
+    event.target.className !== "dropdownContent") {
+      dropdown.classList.remove("show");      
+    }
+
+
+  }
+
+  /*onmouseover="dropDownMenuShow(event)"
+  onmouseout="dropDownMenuHide(event)"*/
+
+
+
