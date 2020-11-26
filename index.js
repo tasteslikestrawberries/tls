84f3cc8df710669c209966b87fb686a24bac5043
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2020-11-26 22:08:02 +0100

    abcdef

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function dropDownMenuShow(event) {
-  console.log(event.target)
-  console.log(event.currentTarget)
-    var dropdown = document.getElementById("dropdown");
-    var dropdownClasses = dropdown.className.split(" ");
-    var index = dropdownClasses.indexOf("show");
-    //console.log(target.className === "dropbutton");
-    //console.log(event.target);
-    //console.log(index);
-    if (index < 0 && event.target.className === "dropbutton") {
-      dropdown.classList.toggle("show");
+    console.log(event.target)
+    console.log(event.currentTarget)
+      var dropdown = document.getElementById("dropdown");
+      var dropdownClasses = dropdown.className.split(" ");
+      var index = dropdownClasses.indexOf("show");
+      //console.log(target.className === "dropbutton");
+      //console.log(event.target);
       //console.log(index);
-    } else if (index >= 0) {
-      //console.log(event.target.className);
+      if (index < 0 && event.target.className === "dropbutton") {
+        dropdown.classList.toggle("show");
+        //console.log(index);
+      } else if (index >= 0) {
+        //console.log(event.target.className);
+        if (event.target.className !== "dropbutton" && 
+          event.target.className !== "dropdownContent") {
+            dropdown.classList.toggle("show");      
+          }
+      }
+    }
+  
+    function dropDownMenuHide(event) {
+      console.log(event.target);
+      var dropdown = document.getElementById("dropdown");
       if (event.target.className !== "dropbutton" && 
-        event.target.className !== "dropdownContent") {
-          dropdown.classList.toggle("show");      
-        }
+      event.target.className !== "dropdownContent") {
+        dropdown.classList.remove("show");      
+      }
+  
+  
     }
-  }
-
-  function dropDownMenuHide(event) {
-    console.log(event.target);
-    var dropdown = document.getElementById("dropdown");
-    if (event.target.className !== "dropbutton" && 
-    event.target.className !== "dropdownContent") {
-      dropdown.classList.remove("show");      
-    }
-
-
-  }
-
-  /*onmouseover="dropDownMenuShow(event)"
-  onmouseout="dropDownMenuHide(event)"*/
-
-
-
+  
+    /*onmouseover="dropDownMenuShow(event)"
+    onmouseout="dropDownMenuHide(event)"*/
+  
+  
+  
+  
---
+++ train.js
@@ -0,0 +1,5 @@
+<script>function heart() {
+    alert("Thank you. You will get an e-mail with your new password soon.");
+    return true;
+}
+</script>
