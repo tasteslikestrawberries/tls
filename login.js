cbd664c43890131c282817a264e9b4729e4bd313
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2020-12-09 21:05:49 +0100

    changes

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
 // Below function Executes on click of login button.
 function validate(){
-var username = document.getElementByName("Username").value;
-var password = document.getElementByName("Password").value;
-if ( username == "formget" && password == "1234"){
+var username = document.getElementById("Username").value;
+var password = document.getElementById("Password").value;
+if ( username === "formget" && password === "1234"){
 alert ("Login successfully");
 window.location = "success.html"; // Redirecting to other page.
 return false;
