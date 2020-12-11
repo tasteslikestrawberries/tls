fe3ca3cb70649e5c3a5dcf92fad73ad54c01dc14
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2020-12-11 20:12:06 +0100

    DFG
    DFDFH

--- login.js
+++ login.js
@@ -1,10 +1,20 @@
 // Below function Executes on click of login button.
-function validate(){
+function validate(event){
 var username = document.getElementById("Username").value;
 var password = document.getElementById("Password").value;
 if ( username === "formget" && password === "1234"){
-alert ("Login successfully");
+    alert ("Login successful!");
 window.location = "success.html"; // Redirecting to other page.
-return false;
+
+event.stopPropagation();
+event.preventDefault();
+
 }
+else
+{ 
+alert("Login unsuccessful!")
+;
 }
+
+}
+
