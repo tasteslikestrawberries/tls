ea341da47e4b163b99f5c7879a026d95a209b7b1
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-01-10 00:16:03 +0100

    finisheddesign

--- NEWLAYOUT/script.js
+++ NEWLAYOUT/script.js
@@ -2,4 +2,19 @@ function menuFunction(x) {
     x.classList.toggle("change");
   }
 
+  (function() {
+
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
   
+    function showNextQuote() {
+      ++quoteIndex;
+      quotes.eq(quoteIndex % quotes.length)
+        .fadeIn(2000)
+        .delay(2000)
+        .fadeOut(2000, showNextQuote);
+    }
+  
+    showNextQuote();
+  
+  })();
