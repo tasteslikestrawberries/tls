c09c69fea5ba26ac427bdef2b3c698ec1fb90c1c
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-01-10 14:38:21 +0100

    navbar play

--- NEWLAYOUT/script.js
+++ NEWLAYOUT/script.js
@@ -1,14 +1,20 @@
 function menuFunction(x) {
-    x.classList.toggle("change");
+  x.classList.toggle("change");
+}
+
+  
+function dropDown() {
+  var x = document.getElementById("dropdown-content");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
-
-  
-
+}
     
     
   
-    
-      
+ 
       
       
         
