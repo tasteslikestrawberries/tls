839ec238e727df398874919639617c53a968c76b
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-01-11 17:47:39 +0100

    Ferris Wheel Layout

--- ferriswheel/script.js
+++ ferriswheel/script.js
@@ -1,6 +1,6 @@
 var menu = document.getElementById('menu');
 menu.addEventListener("click", dropdown);
-console.log(menu);
+
 function dropdown() {
   
    var x = document.getElementById('dropdown-content');
