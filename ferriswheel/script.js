6cc1dcb9530410c5400fa65063a8cb127abccaf9
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-01-11 17:22:06 +0100

    Ferris Wheel Layout

--- ferriswheel/script.js
+++ ferriswheel/script.js
@@ -1,20 +1,13 @@
 function dropdown() {
   var x = document.getElementById('dropdown-content');
-  if (x.style.display === 'none') {
+  if (window.getComputedStyle(x).getPropertyValue('display') === 'none') {
     x.style.display = 'block';
      document.getElementById("menu").classList.toggle("change");
 
   } else {
     x.style.display = 'none';
-    document.getElementById("menu").classList.toggle("change");
+    
   }
-}
-    
 
-      
-      
-        
-        
-    
-  
-    
+}
+
