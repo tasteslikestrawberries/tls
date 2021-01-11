62e1fa0a34eb50a3cabd76eecb9916c905194bb3
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-01-11 17:48:34 +0100

    Ferris Wheel Layout

--- ferriswheel/script.js
+++ ferriswheel/script.js
@@ -2,12 +2,12 @@ var menu = document.getElementById('menu');
 menu.addEventListener("click", dropdown);
 
 function dropdown() {
-  
    var x = document.getElementById('dropdown-content');
    document.getElementById("menu").classList.toggle("change");
    
   if (window.getComputedStyle(x).getPropertyValue('display') === 'none') {
     x.style.display = 'block';
+    
   } else {
     x.style.display = 'none';
   }
