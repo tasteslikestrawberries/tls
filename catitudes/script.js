48f0425a874039cdb25f58935355de8e46df86fb
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-01-12 23:14:56 +0100

    Catitudes Layout

---
+++ catitudes/script.js
@@ -0,0 +1,14 @@
+var menu = document.getElementById('menu');
+menu.addEventListener("click", dropdown);
+
+function dropdown() {
+   var x = document.getElementById('dropdown-content');
+   document.getElementById("menu").classList.toggle("change");
+   
+  if (window.getComputedStyle(x).getPropertyValue('display') === 'none') {
+    x.style.display = 'block';
+    
+  } else {
+    x.style.display = 'none';
+  }
+}
