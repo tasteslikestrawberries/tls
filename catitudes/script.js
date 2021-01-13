994b2acb4dc05b21dfea0ef989b8873faafe815d
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-01-13 12:50:25 +0100

    changes

--- catitudes/script.js
+++ catitudes/script.js
@@ -2,7 +2,7 @@ var menu = document.getElementById('menu');
 menu.addEventListener("click", dropdown);
 
 function dropdown() {
-   var x = document.getElementById('dropdown-content');
+   var x = document.getElementsByClassName('dropdown-content')[0];
    document.getElementById("menu").classList.toggle("change");
    
   if (window.getComputedStyle(x).getPropertyValue('display') === 'none') {
@@ -12,3 +12,36 @@ function dropdown() {
     x.style.display = 'none';
   }
 }
+
+
+
+
+$(function () {
+  'use strict';
+
+  function DotNav($nav){
+    var $dots = $nav.find('li'),
+        current = 0;
+
+    $dots.click(function (el) {
+
+      el.preventDefault();
+      el.stopPropagation()
+      var $dot = $(this),
+          idx = $dots.index($dot);
+
+      if( idx !== current ){
+        $dots.removeAttr('class');
+
+        setTimeout( function() {
+          $dot.addClass('is-current');
+          current = idx;
+        }, 25 );
+      }
+    })
+  }
+
+  $('.dotstyle').each(function () {
+    new DotNav($(this));
+  });
+});
