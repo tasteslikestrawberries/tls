70d8e87e5549cb61897d30f4b5226ae26f8d45b8
Anja <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-01-13 18:11:52 +0100

    Catitudes

--- catitudes/script.js
+++ catitudes/script.js
@@ -14,34 +14,3 @@ function dropdown() {
 }
 
 
-
-
-$(function () {
-  'use strict';
-
-  function DotNav($nav){
-    var $dots = $nav.find('li'),
-        current = 0;
-
-    $dots.click(function (el) {
-
-      el.preventDefault();
-      el.stopPropagation()
-      var $dot = $(this),
-          idx = $dots.index($dot);
-
-      if( idx !== current ){
-        $dots.removeAttr('class');
-
-        setTimeout( function() {
-          $dot.addClass('is-current');
-          current = idx;
-        }, 25 );
-      }
-    })
-  }
-
-  $('.dotstyle').each(function () {
-    new DotNav($(this));
-  });
-});
